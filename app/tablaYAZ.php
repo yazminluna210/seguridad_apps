d7e3ad966b939545c0d70d6ad4dff4ea57f49239
Yazmin <yazmin.luna@supernovaapps.com.mx>
2021-07-15 17:47:17 -0500

    camponuevo

--- app/tablaYAZ.php
+++ app/tablaYAZ.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class tablaYAZ extends Model
 {
-    //
+    protected $primarykey="id";
+    public $timestamps=false;
+    protected $table = "tablaYAZ";
 }
