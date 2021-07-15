b88e8a060a8c6fef44156c213f3dc2bf749c52c4
Yazmin <yazmin.luna@supernovaapps.com.mx>
2021-07-15 18:01:57 -0500

    controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,9 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+
+use App\tablaYAZ;
 
 
 use Illuminate\Support\Facades\DB;
