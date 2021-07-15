fc8d6c477e156c2f728e28716f926b51b48ecca8
Yazmin <yazmin.luna@supernovaapps.com.mx>
2021-07-14 22:02:51 -0500

    palomita2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 2
+                    Laravel 3
                 </div>
 
                 <div class="links">
