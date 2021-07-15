36294afdcd906070fd213157b217c759e5ac941f
Yazmin <yazmin.luna@supernovaapps.com.mx>
2021-07-15 18:33:53 -0500

    yaz22

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel yaz
+                    Laravel yaz 22
                 </div>
 
                 <div class="links">
