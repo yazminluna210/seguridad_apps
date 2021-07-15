282a4eb88aef5cd845e8c33b05306a5addc413c3
Yazmin <yazmin.luna@supernovaapps.com.mx>
2021-07-14 21:25:19 -0500

    hola commit

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\Controller::class, 'rutaYAZ']);
+//Route::get('/', [\App\Http\Controllers\Controller::class, 'rutaYAZ']);
+
+
+Route::get('/', function(){
+    return view("welcome");
+});
